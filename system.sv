e4f536960559d29c9f1ee17386192d82e86c2753
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-06-14 16:56:29 +0200

    Update system.sv

--- system.sv
+++ system.sv
@@ -3,14 +3,19 @@
 module system
 (
 input logic clk,
-input logic rst
+input logic rst,
+
+output logic S0,
+output logic S1,
+output logic IOMn,
+output logic RDn
 );
 
 wire [7:0] bus;
 wire [15:0] address_bus;
-logic S0,S1,WRn,RDn,IOMn,ready;
 
-cpu8080 U1(.clock(clk),.reset_in(rst),.ADD(address_bus),.DATA(bus),.RDn(RDn),.READY(ready));
-rom8775 U2(.CLK(clk),.RESET(rst),.ADD(address_bus),.AD(bus),.READY(ready),.RDn(RDn));
+
+cpu8080 U1(.clock(clk),.reset_in(rst),.ADD(address_bus),.DATA(bus),.RDn(RDn),.READY(ready),.IO_Mn(IOMn),.S0(S0),.S1(S1));
+rom8775 U2(.CLK(clk),.RESET(rst),.ADD(address_bus),.AD(bus),.READY(ready),.RDn(RDn),.IO_Mn(IOMn));
 
 endmodule
