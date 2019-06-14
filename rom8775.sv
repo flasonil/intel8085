d8988fe96105a1e77f16e1fb90a9e93c2ccc528f
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-06-14 16:56:09 +0200

    Update rom8775.sv

--- rom8775.sv
+++ rom8775.sv
@@ -3,13 +3,14 @@ module rom8775
 inout logic [7:0] AD,
 input logic [15:0] ADD,
 input logic RDn,
+input logic IO_Mn,
 
 input logic CLK,
 input logic RESET,
 
 output logic READY
 );
-
+logic MemRead;
 logic [7:0] datao,address;
 
 always@(negedge CLK)begin
@@ -35,7 +36,7 @@ endcase
 //READY <= 1'b1;
 end*/
 
-
-assign AD = RDn ? 8'bzzzzzzzz : datao;
+assign MemRead = ~(~RDn & ~IO_Mn);
+assign AD = MemRead ? 8'bzzzzzzzz : datao;
 
 endmodule
