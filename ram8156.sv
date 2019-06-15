de82a5bf7b5dc9a1283cdf5240dc12e842bf5668
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-06-15 19:04:16 +0200

    MV and MVI implemented

--- ram8156.sv
+++ ram8156.sv
@@ -12,21 +12,24 @@ input logic RDn,
 input logic IOMn
 );
 logic MemRead,MemWrite;
-
+integer i;
 logic [7:0] datao;
 logic [7:0] memory[0:256];
 
 always@(negedge clk) begin
-	/*if(rst)
-	else begin*/
+	if(rst) begin
+		for(i=0;i<256;i++)
+			memory[i] <= 8'b00000000;
+	end else begin
 	if(MemWrite)
 		memory[address] <= data;
 	else datao <= memory[address];
+	end
 end
 
-assign MemRead = ~(~RDn & ~IOMn);
-assign MemWrite = ~(~WRn & ~IOMn);
+assign MemRead = ((~RDn & ~IOMn)&~CSn);
+assign MemWrite = ((~WRn & ~IOMn)&~CSn);
 
-assign data = MemRead ? 8'bzzzzzzzz : datao;
+assign data = (MemRead) ? datao : 8'bzzzzzzzz;
 
 endmodule
