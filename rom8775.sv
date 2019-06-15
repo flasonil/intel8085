3ffa3c5194a70a6f313558419c71b0e011d9c7a9
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-06-15 19:05:35 +0200

    MOV and MVI implemented

--- rom8775.sv
+++ rom8775.sv
@@ -1,9 +1,10 @@
 module rom8775
 (
 inout logic [7:0] AD,
-input logic [15:0] ADD,
+input logic [7:0] ADD,
 input logic RDn,
 input logic IO_Mn,
+input logic CSn,
 
 input logic CLK,
 input logic RESET,
@@ -18,25 +19,13 @@ always@(negedge CLK)begin
 	else
 	case(ADD)
 
-	`include "test.rom" // get contents of memory
+	`include "test2.rom" // get contents of memory
 	default datao = 8'h76; // hlt
 
 endcase
 end
 
-/*always@(address) begin
-
-
-case(address)
-
-	`include "test.rom" // get contents of memory
-	default datao = 8'h76; // hlt
-
-endcase
-//READY <= 1'b1;
-end*/
-
-assign MemRead = ~(~RDn & ~IO_Mn);
-assign AD = MemRead ? 8'bzzzzzzzz : datao;
+assign MemRead = ((~RDn & ~IO_Mn)&~CSn);
+assign AD = (MemRead) ? datao : 8'bzzzzzzzz;
 
 endmodule
