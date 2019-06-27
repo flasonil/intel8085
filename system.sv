0f06fa759cb2c7d8151cc4f82636972b3d29bf3a
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-06-27 16:59:41 +0200

    Update system.sv

--- system.sv
+++ system.sv
@@ -36,6 +36,10 @@ output logic WRn
 wire [7:0] bus;
 wire [15:0] address_bus;
 
+// always@(ALE or bus)begin
+//	if(ALE) address <= {address_bus,bus};
+//end
+
 
 cpu8080 U1(.clock(clk),.reset_in(rst),.ADD(address_bus),.DATA(bus),.RDn(RDn),.READY(ready),.IO_Mn(IOMn),.S0(S0),.S1(S1),.WRn(WRn));
 rom8775 U2(.CLK(clk),.RESET(rst),.ADD(address_bus[7:0]),.AD(bus),.READY(ready),.RDn(RDn),.IO_Mn(IOMn),.CSn(~(~address_bus[15]&~address_bus[14]&~address_bus[13]&~address_bus[12]&~address_bus[11]&~address_bus[10]&~address_bus[9]&address_bus[8])));
